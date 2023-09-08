07787e5ee00459f29aa1478c99342a5d095cf147
Yasen Kostov <ykostov@pm.me>
2023-09-08 11:40:27 +0300

    initial commit

---
+++ app/src/main/java/net/bratac/tennis/MainActivity.java
@@ -0,0 +1,55 @@
+package net.bratac.tennis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mywebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView = (WebView) findViewById(R.id.webview);
+        mywebView.setWebChromeClient(new WebChromeClient());
+        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.loadUrl("https://tennismasa.online/");
+        mywebView.addJavascriptInterface(this, "Android");
+        WebSettings webSettings = mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setDomStorageEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            mywebView.setForceDarkAllowed(true);
+        }
+
+    }
+
+    public class mywebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        if (mywebView.canGoBack()) {
+            mywebView.goBack();
+        }
+    }
+}
